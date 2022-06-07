7dace4584e86f4ca20261753a92f5b21074cd152
Maikhanh Ho <mth200010@utdallas.edu>
2022-06-07 01:09:45 -0500

    Sand Box
    Completed basic mechanics

---
+++ Assets/2DGamekit/Scripts/Core/Checkpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit2D
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class Checkpoint : MonoBehaviour
+    {
+        public bool respawnFacingLeft;
+        public AudioClip clip;
+
+        bool SFXplayed = false;
+
+        private void Reset()
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true; 
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            PlayerCharacter c = collision.GetComponent<PlayerCharacter>();
+            if(c != null)
+            {
+                c.SetChekpoint(this);
+                if (clip && SFXplayed == false)
+                {
+                    AudioSource.PlayClipAtPoint(clip, transform.position);
+                    SFXplayed = true;
+                }
+            }
+        }
+    }
+}
